4a9cf8cc8421640c8644dbeb18d487ab8f0429ce
matvandrei <mateusfellype.vandrei@gmail.com>
2026-09-23 21:34:54 -0300

    começo da logica de converção

--- scripts.js
+++ scripts.js
@@ -1,1 +1,22 @@
+const convertButton = document.querySelector(".convert-button")
 
+convertButton.addEventListener("click", function(){
+    const inputCurrencyValue = document.querySelector(".input-value").value
+    const currencyValueToConvert = document.querySelector(".currency-value-to-convert")
+    const currencyValueConverted = document.querySelector(".currency-value")
+
+    const dolarToday = 5.2
+
+    const convertedValue = inputCurrencyValue / dolarToday
+    console.log(convertedValue)
+
+    currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
+        style: "currency",
+        currency: "BRL"
+    }).format(inputCurrencyValue)
+
+    currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD"
+    }).format(convertedValue)
+})
